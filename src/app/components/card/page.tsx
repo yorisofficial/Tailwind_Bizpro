8293813a45810ddd6da0a7dcd2843975b8d580c5
ADE YOGA WIYADNYANA <yorisofficial@gmail.com>
2023-10-22 21:27:51 +0800

    Update page.tsx

--- src/app/components/card/page.tsx
+++ src/app/components/card/page.tsx
@@ -36,8 +36,8 @@ function Card() {
                 className="mySwiper w-full h-full py-8"
             >
                 {dataPakages.map((item) => (
-                    <SwiperSlide key={item.id} className='flex flex-shrink-0 gap-4 border-2 bg-theme border-[#f4f4f4] rounded-xl p-4 drop-shadow-sm hover:bg-basic hover:drop-shadow-xl'>
-                        <img src={item.gambar} alt="" className='object-cover w-full h-full rounded-md' />
+                    <SwiperSlide key={item.id} className='flex flex-shrink-0 gap-4 border-2 bg-theme border-[#f4f4f4] rounded-xl p-4 drop-shadow-sm hover:bg-basic hover:drop-shadow-xl z-20'>
+                        <img src={item.gambar} alt="gambar" className='object-cover w-full h-full rounded-md' />
                         <h2 className='text-xl md:text-3xl font-bold mt-4'>{item.tittle}</h2>
                         <div className="flex w-full items-center justify-between mt-4">
                             <div className="flex items-center flex-col">
