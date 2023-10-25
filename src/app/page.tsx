b01ce247f9d40bd55f1d7fd24c102153c1d89858
ADE YOGA WIYADNYANA <yorisofficial@gmail.com>
2023-10-26 00:36:18 +0800

    Update resource

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <section id="hero-section">
           <div className="hero-section bg-theme w-full h-full grid grid-cols-1 xl:grid-cols-2 items-center justify-center px-[5%] py-32 md:px-32">
             <div className="w-full h-full xl:order-2 md:mt-20 py-10 md:py-0">
-              <Image src="./images/hero-img.png" alt="hero images" height={500} width={500} className="w-full h-full object-contain" />
+              <Image src="./images/hero-img.webp" alt="hero images" height={500} width={500} className="w-full h-full object-contain" />
             </div>
             <div className="w-full h-full flex justify-center items-center flex-col xl:items-start gap-8 md:mt-20">
               <h1 className="text-5xl md:text-6xl font-bold text-center xl:text-start xl:w-full">Perf<span className="relative">ec<Image src="./images/acc-heading.svg" alt="play button" height={20} width={20} className={`${styleAcc}`} /></span>t place for your business promotion</h1>
